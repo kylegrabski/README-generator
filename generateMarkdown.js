7fbea63435b0b659a49004fb16ecb22692d0bef5
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-18 16:52:27 -0700

    create prompts, generateMarkDown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -11,10 +11,99 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown (data) {
 
-`;
-}
+
+  // use let instead, create empty variables. if they exist, push into variables.
+let github;
+
+if (data.github) {
+  github = "Github: " + data.github;
+}else{
+   github = "";
+};
+
+let email;
+
+if (data.email) {
+  email = "Email: " + data.email;
+}else{
+   email = "";
+};
+
+let website;
+
+if (data.website) {
+  website = "Website: " + data.website;
+}else{
+   website = "";
+};
+
+
+
+
+const title = data.title;
+const description = data.description;
+const installInst = data.installInst;
+const installReq = data.installReq;
+
+return `# ${title}
+
+## Description
+<!-- create line breaks -->
+${description}
+
+
+## Table of contents  
+
+>- [Installation Requirements](#installation-requirements)  
+>- [Install Instructions](#install-instructions)  
+>- [Usage](#usage)  
+>- [License](#license)  
+>- [Contribute](#contribute)  
+>- [Tests](#tests)  
+>- [Questions](#questions)   
+
+&nbsp;
+
+## Installation Requirements 
+${installReq}
+
+&nbsp;
+
+## Install Instructions 
+${installInst}
+
+&nbsp;
+
+## Usage
+- __Install__ _sample document_
+- __Enter__ _this phrase_ in the command line
+- Enjoy the capabilities of this app!
+
+&nbsp;
+
+## License
+<!-- displays badge near top of readme -->
+Here is where your license details will be if applicable.
+
+&nbsp;
+
+## Contribute
+You can contribute to the evelopment of the app here!  
+${github}  
+${email}  
+${website}  
+&nbsp;
+
+## Tests
+What tests have you done with this app?
+
+&nbsp;
+
+## Questions
+This will display your Github profile and email address.
+`
+};
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,38 +1,109 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+
+const questions = inquirer
+  .prompt([
     {
-        type: "input",
-        message: "What is the title of your app?",
-        name: "title",
-      },{
-          type: "confirm",
-          message: "Required installation npm's?",
-          name: "confirmInstall"
+      type: "input",
+      message: "What is the title of your app?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Type a short description of your app",
+      name: "description",
+    },
+    {
+      // CHANGE THIS TO BE ABLE TO WRITE A LIST
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "installInst",
+    },
+    {
+      type: "confirm",
+      message: "Required installation npm's?",
+      name: "confirmInstall",
+    },
+    {
+      type: "input",
+      message: "What is required for installation of your app?",
+      name: "installReq",
+      when: function (answer) {
+        return answer.confirmInstall;
       },
-      {
-        type: "input",
-        message: "List all that needs to be done for installation.",
-        name: "install",
-        when: function (answer) {
-            return answer.confirmInstall;
-        }
+    },
+    {
+      type: "confirm",
+      message: "Share your github?",
+      name: "shareGithub",
+    },
+    {
+      type: "input",
+      message: "Github Profile: ",
+      name: "github",
+      when: function (answer) {
+        return answer.shareGithub;
       },
-      {
-        type: "input",
-        message: "Type a short description of your app",
-        name: "description",
-      }
-]);
+    },
+    {
+      type: "confirm",
+      message: "Share your github?",
+      name: "shareGithub",
+    },
+    {
+      type: "input",
+      message: "Github Profile: ",
+      name: "github",
+      when: function (answer) {
+        return answer.shareGithub;
+      },
+    },
+    {
+      type: "confirm",
+      message: "Share your Email?",
+      name: "shareEmail",
+    },
+    {
+      type: "input",
+      message: "Email: ",
+      name: "email",
+      when: function (answer) {
+        return answer.shareEmail;
+      },
+    },
+    {
+      type: "confirm",
+      message: "Share your Website?",
+      name: "shareWebsite",
+    },
+    {
+      type: "input",
+      message: "Website: ",
+      name: "website",
+      when: function (answer) {
+        return answer.shareWebsite;
+      },
+    },
+  ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  .then((data) => {
+    writeToFile("README2.md", generateMarkdown(data));
+  });
+
+
+
+//  create README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("...NEW README.md FILE CREATED");
+  });
+};
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
-
